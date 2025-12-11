70b469224c9550c1e035045d1b90df3e8c712cf3
ShenHsinen <112969339+ShenHsinen@users.noreply.github.com>
2025-12-11 14:22:24 +0800

    Update NAS.py

--- NAS.py
+++ NAS.py
@@ -21,7 +21,7 @@ if pid:
         found = False
         for idx, row in subset.iterrows():
             # 找出 G~N 欄位為 'Y' 的欄位
-            cols_with_Y = col for col in cols_to_check if row[col] == 'Y'
+            cols_with_Y = [col for col in cols_to_check if row[col] == 'Y']
             if cols_with_Y:
                 found = True
                 st.write({
