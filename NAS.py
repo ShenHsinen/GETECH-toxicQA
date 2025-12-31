c9b1b137500daf151c2f379588bacaeb24ffb6f6
ShenHsinen <112969339+ShenHsinen@users.noreply.github.com>
2025-12-31 08:55:33 +0800

    Update NAS.py

--- NAS.py
+++ NAS.py
@@ -61,6 +61,6 @@ if pid:
             if doc_subset.empty:
                 st.info("找不到對應的文件需求資料")
             else:
-                st.dataframe(doc_subset.reset_index(drop=True))
+                st.table(doc_subset.reset_index(drop=True))
         else:
             st.success("此產品無需準備任何申請文件")
