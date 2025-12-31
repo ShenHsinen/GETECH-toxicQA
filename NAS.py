3fd6b2f1c245dee30236a7f54764ee494cbfa3e9
ShenHsinen <112969339+ShenHsinen@users.noreply.github.com>
2025-12-31 09:13:07 +0800

    Update NAS.py

--- NAS.py
+++ NAS.py
@@ -28,7 +28,7 @@ if pid:
             if cols_with_Y:
                 results.append({
                     "Cas No.": row['CAS NO'],
-                    "成分名稱": row['成分名稱']
+                    "成分名稱": row['成分名稱'],
                     "濃度": f"{row.get('濃度', '')}{row.get('單位', '')}",
                     "類別": ", ".join(cols_with_Y),
                     "申請文件類別": row['申請文件類別'],
