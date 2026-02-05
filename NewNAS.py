e2b560b714af5f3fde53cea0d9265c66b3dee89c
ShenHsinen <112969339+ShenHsinen@users.noreply.github.com>
2026-02-05 17:27:09 +0800

    Update NewNAS.py

--- NewNAS.py
+++ NewNAS.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(
+    page_title="毒化物查詢系統",
+    layout="wide"
+)
+
 st.title("🔍 毒化物查詢系統")
 
 # -------------------- 讀取資料 --------------------
