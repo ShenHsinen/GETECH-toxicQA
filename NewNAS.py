f58b1acae99ebcab1217f45f9568ac4217aea1da
ShenHsinen <112969339+ShenHsinen@users.noreply.github.com>
2026-02-10 15:38:42 +0800

    Update NewNAS.py

--- NewNAS.py
+++ NewNAS.py
@@ -10,15 +10,15 @@ st.set_page_config(
 
 st.title("🔍 毒化物查詢系統")
 
-col1, col2 = st.columns([3, 1])
+col1, col2 = st.columns([4, 1], vertical_alignment="center")
 
 with col1:
-    st.info("📣 請協助填寫 1 分鐘使用回饋，幫助我們優化系統")
+    st.markdown("📣 **請協助填寫 1 分鐘使用回饋，幫助我們優化系統**")
 
 with col2:
     st.link_button(
         "📝 填寫回饋",
-        "https://forms.cloud.microsoft/r/5GRAJn4Xry"
+        "https://forms.gle/你的問卷連結"
     )
 
 
